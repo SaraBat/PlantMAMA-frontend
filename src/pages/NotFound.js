cf71546924ebbe944c392a8e17cc6c499e290042
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-05-29 19:45:45 +0200

    notFoundg page route back home created

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
+  const navigate = useNavigate();
+  const onNotFoundButtonClick = () => {
+    navigate('/');
+  };
   return (
     <div>
-      Lalalalalala
+      <button type="button" onClick={onNotFoundButtonClick}> Back to homepage </button>
     </div>
   )
 }
