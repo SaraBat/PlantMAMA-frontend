ef7f71ae62fa24ad53a036ed72d20f53b80de569
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-05-29 16:13:55 +0200

    react and reduc libraries installed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from 'components/Main';
+import NotFound from 'components/NotFound';
+import Login from 'components/Login';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import user from 'reducers/user';
+import thoughts from 'reducers/thoughts';
+import { Provider } from 'react-redux';
 
 export const App = () => {
   return (
