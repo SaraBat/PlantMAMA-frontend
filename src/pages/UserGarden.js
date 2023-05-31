4f0bcca2e126ad4fbd9a9f5d77fe175cb4bb54d1
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-05-31 22:43:07 +0200

    fixed bug

--- src/pages/UserGarden.js
+++ src/pages/UserGarden.js
@@ -3,7 +3,25 @@ import React from 'react'
 export const UserGarden = () => {
   return (
     <div>
-      Lalalalalala
+      <ul>
+        <li> Plant mama/papa name & info </li>
+        <li> Wether forecast for their location (rain, humidity)</li>
+        <li> Garden grid:
+        Plant list (with overview of each plant that is link to go to plant profile page)
+        </li>
+        <li> Plant name (ie: Sally)</li>
+        <li> plant profile picture (?)</li>
+        <li> last watered (?) + countdown to next watering (?)</li>
+      </ul>
+      <ul>
+        <li> plus button to add plant takes you to pop up</li>
+        <li> Insert plant name</li>
+        <li> Choose plant species</li>
+        <li> Birthday</li>
+        <li> upload plant picture</li>
+        <li> Last watered</li>
+        <li> Last soil change</li>
+      </ul>
     </div>
   )
 }
