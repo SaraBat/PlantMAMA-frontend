0191c0beb36e6ccefd749337cac83b5ffbdf9712
AntoniaGranit <antonia.granit@gmail.com>
2023-06-08 12:00:49 +0300

    install axios and initial file input jsx code

--- src/pages/EditPlantProfile.js
+++ src/pages/EditPlantProfile.js
@@ -1,6 +1,6 @@
 /* eslint-disable object-shorthand */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { API_URL } from 'utils/BackendUrl';
@@ -10,8 +10,10 @@ export const EditPlantProfile = () => {
   const accessToken = useSelector((store) => store.user.accessToken);
   const username = useSelector((store) => store.user.username);
   const { plantId } = useParams();
+  const fileInput = useRef();
   const [plantname, setPlantname] = useState('');
   const [species, setSpecies] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
   const [birthday, setBirthday] = useState('');
   // dispatch to put access token into main component
   const dispatch = useDispatch();
@@ -40,9 +42,10 @@ export const EditPlantProfile = () => {
         console.log(plantname);
         console.log(species);
         console.log(birthday);
+        console.log(imageUrl);
         navigate(-2)
         // eslint-disable-next-line max-len
-        dispatch(plants.actions.editSinglePlant({ plantId: plantId, plantname: plantname, species: species, birthday: birthday }));
+        dispatch(plants.actions.editSinglePlant({ plantId: plantId, plantname: plantname, species: species, birthday: birthday, imageUrl: imageUrl }));
         console.log('dispatch sent');
         dispatch(plants.actions.setError(null));
       });
@@ -50,24 +53,32 @@ export const EditPlantProfile = () => {
   return (
     <>
       <form onSubmit={onFormSubmit}>
-        <label htmlFor="plantnamee"> New plantname </label>
+        <label htmlFor="plantnamee"> Change plantname </label>
         <input
           type="text"
           id="plantname"
           value={plantname}
-          onChange={(e) => setPlantname(e.target.value)} />
-        <label htmlFor="species"> New species </label>
+          onChange={(e) => setPlantname(e.target.value)} /><br /><br />
+        <label htmlFor="species"> Change species </label>
         <input
           type="text"
           id="species"
           value={species}
-          onChange={(e) => setSpecies(e.target.value)} />
-        <label htmlFor="birthday"> Add birthday </label>
+          onChange={(e) => setSpecies(e.target.value)} /><br /><br />
+        <label htmlFor="birthday"> Change birthday </label>
         <input
-          type="birthday"
+          type="date"
           id="birthday"
           value={birthday}
-          onChange={(e) => setBirthday(e.target.value)} />
+          onChange={(e) => setBirthday(e.target.value)} /><br /><br />
+        <label htmlFor="imageUrl"> Change picture </label>
+        <input
+          type="file"
+          id="imageUrl"
+          placeholder="Add plant photo"
+          value={imageUrl}
+          ref={fileInput}
+          onChange={(e) => setImageUrl(e.target.value)} /><br /><br />
         <button type="submit"> Submit </button>
       </form>
       <button
