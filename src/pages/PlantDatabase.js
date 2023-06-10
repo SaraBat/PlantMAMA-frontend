fe540e2ff64c87241b2aefcf453c18970ccdf53b
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-06-10 13:06:34 +0200

    plant inspiration page works but returns duplicate plants

--- src/pages/PlantDatabase.js
+++ src/pages/PlantDatabase.js
@@ -1,54 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { REACT_APP_PLANT_API_KEY } from 'utils/BackendUrl';
+import { PlantList } from 'components/PlantList';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const PlantDatabase = () => {
   const navigate = useNavigate();
   const onBackClick = () => {
     navigate(-1);
   }
+  const [param, setParam] = useState(null);
+  const onSetParamClick = (value) => {
+    setParam(value)
+  }
 
-  // we could filter API data by
+  if (!param) {
+    return (
+      <div>
+        <button
+          type="button"
+          value="&poisonous=1"
+          onClick={(event) => onSetParamClick(event.target.value)}> Poisonous
+        </button>
+        <button
+          type="button"
+          value="&edible=1"
+          onClick={(event) => onSetParamClick(event.target.value)}> Edible
+        </button>
+        <button
+          type="button"
+          value="&indoor=1"
+          onClick={(event) => onSetParamClick(event.target.value)}> Indoor
+        </button>
+        <button
+          type="button"
+          value="&watering=minimum"
+          onClick={(event) => onSetParamClick(event.target.value)}> Minimum watering
+        </button>
+        <button
+          type="button"
+          value="&sunlight=full_shade"
+          onClick={(event) => onSetParamClick(event.target.value)}> Dark Mode
+        </button>
+        <button
+          type="button"
+          onClick={onBackClick}> Back
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <div> <PlantList param={param} /> </div>
+    )
+  }
+}
 
-  /*
-  Cycle - optional, string
-   The plant cycle of the species.
-   Options - perennial, annual, biennial, biannual
-   https://perenual.com/api/species-list?key=[YOUR-API-KEY]&cycle=annual
+// we could filter API data by
+
+/*
+  Poisonous - optional, boolean, default is NULL
+  If plant species is poisonous or not
+  https://perenual.com/api/species-list?key=[YOUR-API-KEY]&poisonous=1
   */
-  /*
+/*
+  Edible - optional, boolean, default is NULL
+  If plant species is edible or not for consumption
+  https://perenual.com/api/species-list?key=[YOUR-API-KEY]&edible=1
+  */
+/*
+  Indoor - optional, boolean, default is NULL
+  If plant species is indoors
+  https://perenual.com/api/species-list?key=[YOUR-API-KEY]&indoor=1
+  */
+/*
   Watering - optional, string
    The watering amount of the species.
    Options - frequent, average, minimum, none
    https://perenual.com/api/species-list?key=[YOUR-API-KEY]&watering=frequent
  */
-  /*
+/*
   Sunlight - optional, string
    The sunlight amount of the species.
    Options - full_shade, part_shade, sun-part_shade, full_sun
    https://perenual.com/api/species-list?key=[YOUR-API-KEY]&sunlight=full_sun
  */
-
-  const [plantSpeciesList, setPlantSpeciesList] = useState([]);
-  useEffect(() => {
-    fetch(`https://perenual.com/api/species-list?page=1&key=${REACT_APP_PLANT_API_KEY}&indoor=1`)
-      .then((response) => response.json())
-      .then((data) => setPlantSpeciesList((data.data)))
-  }, []);
-  console.log(plantSpeciesList);
-  return (
-    <div>
-      {plantSpeciesList.map((singleSpecies) => {
-        return (
-          <div key={singleSpecies.id}>
-            <Link to={`/plantdatabase/${singleSpecies.id}`}> Learn more about {singleSpecies.common_name}  </Link>
-          </div>
-        )
-      })}
-      <button
-        type="button"
-        onClick={onBackClick}> Back
-      </button>
-    </div>
-  )
-}
