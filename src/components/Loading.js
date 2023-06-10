5c2e45969c5d3a2f6f0b601005a403580a0bae6d
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-06-10 22:07:24 +0200

    lottie animation for loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web'
 
 export const Loading = () => {
+  const container = useRef(null);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      render: 'svg',
+      loop: true,
+      autoplay: true,
+      // eslint-disable-next-line global-require
+      animationData: require('../assets/plant.json')
+    })
+  }, [])
   return (
     <div>
-      <p>LOADING......</p>
+      <div className="container" ref={container}> </div>
     </div>
   )
 }
